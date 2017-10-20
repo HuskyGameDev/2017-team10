d50dbc1a4c3ad7ffd5e4a5be329da15ffc908478
dcgaines <dcgaines@mtu.edu>
2017-10-19 22:01:48 -0400

    Commented AI movement script

--- Assets/Scripts/SampleAgentScript.cs
+++ Assets/Scripts/SampleAgentScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class SampleAgentScript : MonoBehaviour {
+    // Reads in 2 empty objects as target locations
     public Transform target1;
     public Transform target2;
     NavMeshAgent agent;
-    private bool tar = true;
+    private bool tar = true; // Controls which target is the destination
 
 	// Use this for initialization
 	void Start () {
@@ -16,11 +17,13 @@ public class SampleAgentScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // Set the Nav Agent's destination based on the tar variable
         if (tar)
             agent.SetDestination(target1.position);
         else
             agent.SetDestination(target2.position);
 
+        // Switch targets once the target is reached
         if(Vector3.Distance(agent.transform.position, agent.destination) < agent.stoppingDistance)
         {
             tar = !tar;
